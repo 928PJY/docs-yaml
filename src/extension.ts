75de9fa037e56b288c0117a308ed2ceb69e664b9
Jiayin Pei <jipe@microsoft.com>
2018-09-10 20:27:18 +0800

    backuup

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@ import * as vscode from 'vscode';
 
 import { DocsYamlCompletionProvider } from "./yaml-support/yaml-snippet";
 import { registerYamlSchemaSupport } from './yaml-support/yaml-schema';
+import axios from 'axios';
+import { xhr, XHRResponse, configure as configureHttpRequests, getErrorStatusDescription } from 'request-light'
 import { YAML_SCHEMA_CONFIG_NAME_OF_VSCODE_YAML_EXTENSION, TOC_SCHEMA_FILE, TOC_FILE_GLOBAL_PATTERN } from "./yaml-support/yaml-constant";
 
 // this method is called when your extension is activated
@@ -15,6 +17,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
     await addTocSchemaToConfig();
     await registerYamlSchemaSupport();
+    // monitorSchemaChange();
     subscriptions.forEach((element) => {
         context.subscriptions.push(element);
     }, this);
@@ -33,6 +36,49 @@ async function addTocSchemaToConfigAtScope(configKey: string, value: string, sco
     newValue[configKey] = value;
     await vscode.workspace.getConfiguration().update(YAML_SCHEMA_CONFIG_NAME_OF_VSCODE_YAML_EXTENSION, newValue, scope);
 }
+
+function monitorSchemaChange() {
+    let uri = "https://api.github.com/repos/928PJY/perf-test-repo/commits/extension-test"
+    let lastCommitId = '';
+
+    const options = {
+        method: 'Get',
+        headers: { 'content-type': 'application/x-www-form-urlencoded', 'Authorization': '32ff3a19665ad2e2bb56fb5dffcda1d4746431ff' },
+        url: uri,
+      };
+      axios(options)
+        .then(response => {
+            lastCommitId = response.data.sha;
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+    setInterval(() => {
+        axios(options)
+        .then(response => {
+            if(lastCommitId === '')
+            {
+                lastCommitId = response.data.sha;
+            } else if(lastCommitId !== response.data.sha){
+                lastCommitId = response.data.sha;
+                showReloadPrompt("Reload Window to use new schema");
+            }
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }, 10000)
+}
+
+function showReloadPrompt(msg: string): void {
+    let reload: string = "Reload";
+    vscode.window.showInformationMessage(msg, reload).then(value => {
+        if (value === reload) {
+            vscode.commands.executeCommand("workbench.action.reloadWindowWithExtensionsDisabled");
+        }
+    });
+} 
 
 // this method is called when your extension is deactivated
 export function deactivate() {
